a558595e3cd2c298a9e1c5867053c5e718c809af
HyphenBoberta <tyler.sasso1@gmail.com>
2019-08-21 20:06:03 -0700

    corrected the script, changed background and added blur effect.

--- main.js
+++ main.js
@@ -8,18 +8,19 @@ var count = 1;
 sectionContainer.addEventListener("click", function() { 
     count++;
     if(count === 1){
-        sectionOne.style.display = "block"
+        sectionOne.style.display = "flex"
     } else {sectionOne.style.display = "none"}
     if(count === 2){
-        sectionTwo.style.display = "block"
+        sectionTwo.style.display = "flex"
     } else {sectionTwo.style.display = "none"}
     if(count === 3){
-        sectionThree.style.display = "block"
+        sectionThree.style.display = "flex"
     } else {sectionThree.style.display = "none"}
     if(count === 4){
-        sectionfour.style.display = "block"
-    } else {sectionfour.style.display = "none"}
-    if(count = 5){
+        sectionFour.style.display = "flex"
+    } else {sectionFour.style.display = "none"}
+    if(count > 4){
+        sectionOne.style.display = "flex"
         count = 1;
     }
 })
